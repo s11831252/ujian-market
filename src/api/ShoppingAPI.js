097fc5ab1f7a59583356e8658a381374d66ec7de
s115831252@sina.com <s115831252@sina.com>
2018-11-05 20:05:54 +0800

    百度查地址api更新ak

--- src/api/ShoppingAPI.js
+++ src/api/ShoppingAPI.js
@@ -2,14 +2,14 @@ import http_axios from '../utils/http/axios'
 import http_wx from '../utils/http/wxhttp'
 const http = mpvue_Mode === 'WX' ? http_wx : http_axios;
 
-let BaseHost = "https://market.ujianchina.net/";
-// let BaseHost = "http://192.168.0.86:811/";
+// let BaseHost = "https://market.ujianchina.net/";
+let BaseHost = "http://192.168.0.86:811/";
 
 
 export default {
     baidu_geocoder:param=>{
         //location=35.658651,139.745415
-        let ak="";
+        let ak="yCCZ5HnYGnUoRQNfd0YkTHg8lluFGQRZ";
         return http.get(`http://api.map.baidu.com/geocoder/v2/ak=${ak}&output=json`, param)
     },
     Account_Login: param => {
