2c1648d4bca4a61fc773834cc6c32bf2b4c35867
s115831252@sina.com <s115831252@sina.com>
2018-11-19 21:16:25 +0800

    处理query.scene无值的bug

--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -107,7 +107,8 @@ export default {
   },
   onLoad (query) {
     // scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
-    this.sId = decodeURIComponent(query.scene);
+    if(query.scene)
+      this.sId = decodeURIComponent(query.scene);
     wx.showShareMenu({
       withShareTicket: true
     });
