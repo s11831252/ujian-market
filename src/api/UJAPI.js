e1f71e939af201c5ab8ec5c2a5f5dbde5b352ea3
670953622 <670953622@.qq.com>
2019-01-22 14:36:55 +0800

    使用外网api接口

--- src/api/UJAPI.js
+++ src/api/UJAPI.js
@@ -2,8 +2,8 @@ import http_axios from '../utils/http/axios'
 import http_wx from '../utils/http/wxhttp'
 const http = mpvue_Mode === 'WX' ? http_wx : http_axios;
 
-let BaseHost = "http://192.168.0.119:801/";
-// let BaseHost = "https://app.ujianchina.net/";
+// let BaseHost = "http://192.168.0.119:801/";
+let BaseHost = "https://app.ujianchina.net/";
 
 export default {
     Account_Login: param => {
