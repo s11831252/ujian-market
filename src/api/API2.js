a3abf6b4192c0ab84f98f0e8e9b0cdab0e2f26b6
670953622 <670953622@.qq.com>
2020-03-06 18:43:51 +0800

    调整群描述

--- src/api/API2.js
+++ src/api/API2.js
@@ -6,6 +6,10 @@ let BaseHost = process.env.NODE_ENV != 'development'?"http://localhost:49814/":"
 
 export default {
     groupChat_Create:(groupname,owner,members,desc="",isPublic=false,approval=true)=>{
-        http_wx.get(BaseHost+`groupChat/Create?groupname=${groupname}&members=${members}&owner=${owner}&desc=${desc}&isPublic=${isPublic}&approval=${approval}`)
+       return  http_wx.get(BaseHost+`groupChat/Create?groupname=${groupname}&members=${members}&owner=${owner}&desc=${desc}&isPublic=${isPublic}&approval=${approval}`)
+    },
+    groupChat_ModifyDescription:(groupid,description)=>
+    {
+        return http_wx.get(BaseHost+`groupChat/ModifyDescription?groupid=${groupid}&description=${description}`)
     }
 }
--- src/pages/service/consult.vue
+++ src/pages/service/consult.vue
@@ -19,24 +19,24 @@ import chatItem from "@/pages/service/chat-item";
 import WebIM from "@/utils/hx/WebIM";
 import msgStorage from "./msgstorage";
 import msgType from "./msgtype";
-function ack(receiveMsg){
-	// 处理未读消息回执
-	var bodyId = receiveMsg.id;         // 需要发送已读回执的消息id
-	var ackMsg = new WebIM.message("read", WebIM.conn.getUniqueId());
-	ackMsg.set({
-		id: bodyId,
-		to: receiveMsg.from
-	});
-	WebIM.conn.send(ackMsg.body);
+function ack(receiveMsg) {
+  // 处理未读消息回执
+  var bodyId = receiveMsg.id; // 需要发送已读回执的消息id
+  var ackMsg = new WebIM.message("read", WebIM.conn.getUniqueId());
+  ackMsg.set({
+    id: bodyId,
+    to: receiveMsg.from
+  });
+  WebIM.conn.send(ackMsg.body);
 }
-function onMessageError(err){
-	if(err.type === "error"){
-		wx.showToast({
-			title: err.errorText
-		});
-		return false;
-	}
-	return true;
+function onMessageError(err) {
+  if (err.type === "error") {
+    wx.showToast({
+      title: err.errorText
+    });
+    return false;
+  }
+  return true;
 }
 // function calcUnReadSpot(message){
 // 	let myName = wx.getStorageSync("myUsername");
@@ -59,11 +59,11 @@ export default {
   data() {
     return {
       msg: "",
-      to:"",
       sId: null,
       webSocket: null,
       ChatHistory: [],
-      chatInfo:null
+      chatRoomInfo: null,
+      shopInfo: null
     };
   },
   components: {
@@ -73,80 +73,137 @@ export default {
     ...mapState({
       Ticket: state => state.User.SingleTicket,
       UserInfo: state => state.User.UserInfo
-    })
-  },
-  methods: {
-    SendMsg(){
-      var that = this;
-			let id = WebIM.conn.getUniqueId();
-			let msg = new WebIM.message("txt", id);
-			msg.set({
-				msg: this.msg,
-				from: WebIM.conn.context.userId,
-				to:this.to,
-				roomType: false,
-				chatType: "chat",
-				success(id, serverMsgId){
-          // disp.fire('em.chat.sendSuccess', id, me.data.userMessage);
-          msgStorage.saveMsg(msg,"txt")
-          console.log(`发送消息(id=${id},serverMsgId=${serverMsgId})成功为`,msg)
-          that.msg="";
-				},
-				fail(id, serverMsgId){
-					console.log('发送消息失败')
-				}
-      });
-			msg.setGroup("groupchat");
-      WebIM.conn.send(msg.body);
-      
+    }),
+    to() {
+      if (this.chatRoomInfo) return this.chatRoomInfo.roomId;
+      else return null;
     }
   },
-  mounted() {
+  methods: {
+    SendMsg() {
+      var that = this;
+      if (!this.to) {
+      } else {
+        let id = WebIM.conn.getUniqueId();
+        let msg = new WebIM.message("txt", id);
+        msg.set({
+          msg: this.msg,
+          from: WebIM.conn.context.userId,
+          to: this.to,
+          roomType: false,
+          chatType: "chat",
+          success(id, serverMsgId) {
+            // disp.fire('em.chat.sendSuccess', id, me.data.userMessage);
+            msgStorage.saveMsg(msg, "txt");
+            console.log(
+              `发送消息(id=${id},serverMsgId=${serverMsgId})成功为`,
+              msg
+            );
+            that.msg = "";
+          },
+          fail(id, serverMsgId) {
+            console.log(`发送消息(id=${id},serverMsgId=${serverMsgId})失败`);
+          }
+        });
+        msg.setGroup("groupchat");
+        WebIM.conn.send(msg.body);
+      }
+    }
+  },
+  async mounted() {
+    var that = this;
     this.sId = this.$route.query.sId;
-    this.sName= decodeURI(this.$route.query.sName);
+    var rep = await this.$ShoppingAPI.Shop_GetDetails({ sId: this.sId });
+    if (rep.ret == 0) {
+      this.shopInfo = rep.data;
+    }
+    this.sName = decodeURI(this.$route.query.sName);
     //设置标题
     wx.setNavigationBarTitle({ title: this.sName });
 
     //查询聊天室列表,并仓尝试获取与该店铺的聊天室
-    var listGroup = utils.getItem("listGroup")
-    if(listGroup)
-    {
-      this.chatInfo = listGroup.find(item=>{
-        return item.name==this.UserInfo.Phone+"_"+this.sName
-      })
+    var listGroup = utils.getItem("listGroup");
+    if (listGroup) {
+      this.chatRoomInfo = listGroup.find(item => {
+        return item.name == this.UserInfo.Phone + "_" + this.sName;
+      });
     }
-    //有聊天室
-    if(!this.chatInfo){
-      //没有则创建聊天室
-      var groupname,owner,members,desc="备注"
 
-      groupname=`${this.UserInfo.Phone}_${this.sName}`;
+    //没有则创建聊天室
+    if (!this.chatRoomInfo) {
+     
+      var groupname,
+        owner,
+        members,desc
+      var desc_obj = {
+          store: {
+            sId: this.sId,
+            sNm: this.sName,
+            sLogo:this.shopInfo.sLogo.replace(/\//g, "#")
+          },
+          order: {
+            // orderId: "cc3c1767-684b-4eca-87d1-e09f1dea4b16",
+            // orderNo: "201906181030089564",
+            // state: "交易完成"
+          },
+          buyer: {
+            bId: this.UserInfo.UserId,
+            bPhone: this.UserInfo.Phone,
+            bLogo: this.UserInfo.Portrait.replace(/\//g, "#"),
+            bNm: this.UserInfo.UserName,
+            auth: this.IsCertification?"true":"false"
+          },
+          lastTime: Math.round(new Date().getTime()/1000) 
+      };
+      desc = JSON.stringify(desc_obj);
+      groupname = `${this.UserInfo.Phone}_${this.sName}`;
       //店铺Id为创建人
-      owner=this.sId.replace(/-/g, "")+"_";
+      owner = this.sId.replace(/-/g, "") + "_";
       //把店铺成员一起拉进来
-      this.$ShoppingAPI.ShopEmployee_Get(this.sId).then((rep)=>{
-        if(rep.ret==0)
-        {
-          var _menberArr = rep.data.map(item=>{
+      this.$ShoppingAPI.ShopEmployee_Get(this.sId).then(rep => {
+        if (rep.ret == 0) {
+          var _menberArr = rep.data.map(item => {
             return item.UserId.replace(/-/g, "");
           });
-          _menberArr.push(this.UserInfo.UserId.replace(/-/g, ""))
+          _menberArr.push(this.UserInfo.UserId.replace(/-/g, ""));
           members = _menberArr.join();
-          console.log(groupname,owner,members,desc)
-          this.$API2.groupChat_Create(groupname,owner,members,desc).then(rep2=>{
-            console.log(rep2)
-            if(rep2.ret==0)
-            {
-              this.chatInfo={jid: `888yuezhi-88#ubuild_${rep2.data}@conference.easemob.com`, name: groupname, roomId:  rep2.data};
-              listGroup.push(this.chatInfo)
-              utils.setItem("listGroup",listGroup);
-            }
-          });
+          console.log(groupname, owner, members, desc);
+          this.$API2
+            .groupChat_Create(groupname, owner, members, desc)
+            .then(rep2 => {
+              console.log(rep2);
+              if (rep2.ret == 0) {
+                this.chatRoomInfo = {
+                  jid: `888yuezhi-88#ubuild_${
+                    rep2.data
+                  }@conference.easemob.com`,
+                  name: groupname,
+                  roomId: rep2.data.groupid
+                };
+                listGroup.push(this.chatRoomInfo);
+                utils.setItem("listGroup", listGroup);
+              }
+            });
+        }
+      });
+    } else {
+      WebIM.conn.queryRoomInfo({
+        roomId: this.chatRoomInfo.roomId,
+        success: function(settings, members, fields) {
+          console.log("queryRoomInfo成功",fields);
+          var desc_obj = JSON.parse(fields.description);
+          desc_obj.lastTime = Math.round(new Date().getTime()/1000);
+          var json_obj = JSON.stringify(desc_obj)
+          console.log(json_obj);
+          that.$API2.groupChat_ModifyDescription(that.chatRoomInfo.roomId,json_obj)
+        },
+        error: function(msg) {
+          console.log(msg);
         }
       });
     }
-    console.log(this.chatInfo)
-    this.to = this.chatInfo.roomId
+
+    console.log(this.chatRoomInfo);
   },
   created() {
     var that = this;
@@ -156,44 +213,42 @@ export default {
         // 如果isAutoLogin设置为false，那么必须手动设置上线，否则无法收消息
         // 手动上线指的是调用conn.setPresence(); 如果conn初始化时已将isAutoLogin设置为true
         // 则无需调用conn.setPresence();
-        console.log(message)
+        console.log(message);
         WebIM.conn.setPresence();
-        utils.setItem("myUsername",WebIM.conn.context.userId)
-        
-            WebIM.conn.listRooms({
-                success: function(resp) {
-                    utils.setItem("listGroup",resp)
-                },
-                error: function(e) {
-                    console.log("error:", e);
-                }
-            });
+        utils.setItem("myUsername", WebIM.conn.context.userId);
+
+        WebIM.conn.listRooms({
+          success: function(resp) {
+            utils.setItem("listGroup", resp);
+          },
+          error: function(e) {
+            console.log("error:", e);
+          }
+        });
       },
       onClosed: function(message) {
-
-        console.log("环信onClosed",message)
+        console.log("环信onClosed", message);
       }, //连接关闭回调
       onTextMessage: function(message) {
-				console.log("onTextMessage", message);
-				if(message){
-					if(onMessageError(message)){
-						msgStorage.saveReceiveMsg(message, msgType.TEXT);
-					}
-					// calcUnReadSpot(message);
-					ack(message);
+        console.log("onTextMessage", message);
+        if (message) {
+          if (onMessageError(message)) {
+            msgStorage.saveReceiveMsg(message, msgType.TEXT);
+          }
+          // calcUnReadSpot(message);
+          ack(message);
 
-					// if(message.ext.msg_extension){
-					// 	let msgExtension = JSON.parse(message.ext.msg_extension)
-					// 	let conferenceId = message.ext.conferenceId
-					// 	let password = message.ext.password
-					// 	disp.fire("em.xmpp.videoCall", {
-					// 		msgExtension: msgExtension,
-					// 		conferenceId: conferenceId,
-					// 		password: password
-					// 	});
-					// }
-				}
-        
+          // if(message.ext.msg_extension){
+          // 	let msgExtension = JSON.parse(message.ext.msg_extension)
+          // 	let conferenceId = message.ext.conferenceId
+          // 	let password = message.ext.password
+          // 	disp.fire("em.xmpp.videoCall", {
+          // 		msgExtension: msgExtension,
+          // 		conferenceId: conferenceId,
+          // 		password: password
+          // 	});
+          // }
+        }
       }, //收到文本消息
       onEmojiMessage: function(message) {}, //收到表情消息
       onPictureMessage: function(message) {}, //收到图片消息
